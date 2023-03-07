652e3431f200667fcc1915e6d9d554b58d80d14e
Mariambmb <mariambamba92@live.com>
2023-03-07 15:17:57 +0100

    Merge branch 'master' of https://github.com/lucieargaud/TP1Hibernate.git

--- src/main/java/com/inti/servlet/compagnieServlet.java
+++ src/main/java/com/inti/servlet/compagnieServlet.java
@@ -9,15 +9,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.hibernate.Session;
 
-
+import com.inti.model.Commande;
+import com.inti.model.CompagnieAerienne;
+import com.inti.model.Utilisateur;
+import com.inti.model.Vol;
 import com.inti.util.HibernateUtil;
 
 @WebServlet("/compagnie")
 public class compagnieServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
+	private Logger log = LogManager.getLogger();
 	private Session s;
 	
     public compagnieServlet() {
@@ -30,10 +36,11 @@ public class compagnieServlet extends HttpServlet {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	
+		s = HibernateUtil.getSessionFactory().openSession();
+		log.debug("Connexion à la BDD et configuration d'hibernate depuis commande");
+		
 		this.getServletContext().getRequestDispatcher("/WEB-INF/compagnie.jsp").forward(request, response);
 		
-		s = HibernateUtil.getSessionFactory().openSession();
-	
 	}
 
 	
@@ -44,13 +51,18 @@ public class compagnieServlet extends HttpServlet {
 		try 
 		{
 			s.beginTransaction();
+			log.info("Début enregistrement commande");
 			
+			CompagnieAerienne c1 = new CompagnieAerienne(request.getParameter("id"), request.getParameter("nom"));
+		
+			s.save(c1);
 
 			
 			s.getTransaction().commit();
 		}
 		catch (Exception e) {
 			e.printStackTrace();
+			log.error("Erreur enregistrement utilisateur");
 			
 			s.getTransaction().rollback();
 		}
--- src/main/java/com/inti/servlet/volServlet.java
+++ src/main/java/com/inti/servlet/volServlet.java
@@ -1,20 +1,28 @@
 package com.inti.servlet;
 
 import java.io.IOException;
+import java.time.LocalDate;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.hibernate.Session;
 
+import com.inti.model.Commande;
+import com.inti.model.Utilisateur;
+import com.inti.model.Vol;
 import com.inti.util.HibernateUtil;
 
 @WebServlet("/vol")
 public class volServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
+	private Logger log = LogManager.getLogger();
 	private Session s;
 	
     public volServlet() {
@@ -26,9 +34,10 @@ public class volServlet extends HttpServlet {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 
-		this.getServletContext().getRequestDispatcher("/WEB-INF/vol.jsp").forward(request, response);
-		
 		s = HibernateUtil.getSessionFactory().openSession();
+		log.debug("Connexion à la BDD et configuration d'hibernate depuis commande");
+		
+		this.getServletContext().getRequestDispatcher("/WEB-INF/vol.jsp").forward(request, response);
 		
 	}
 
@@ -40,13 +49,19 @@ public class volServlet extends HttpServlet {
 		try 
 		{
 			s.beginTransaction();
+			log.info("Début enregistrement commande");
 			
+			Vol v1 = new Vol (request.getParameter("id"), LocalDate.parse(request.getParameter("dateD")),request.getParameter("heureD"), 
+					LocalDate.parse(request.getParameter("dateA")), request.getParameter("heureA"));
+			
+			s.save(v1);
 
 			
 			s.getTransaction().commit();
 		}
 		catch (Exception e) {
 			e.printStackTrace();
+			log.error("Erreur enregistrement utilisateur");
 			
 			s.getTransaction().rollback();
 		}
